824d4cac7961f77a086922e00559b8532d697604
Bob Fashan <danilgrachev1992@gmail.com>
2024-03-10 09:54:43 +0300

    tt

---
+++ app_form/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+
+app = FastAPI()
+templates = Jinja2Templates(
+    directory="C:/Users/Bob/VS Project/forms_bot/test_hh_bot/app_form/src/"
+)
+
+
+class Item(BaseModel):
+    name: str
+    description: str
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    print(f"Received item: {item}")
+    return {"message": "Item received"}
+
+
+@app.get("app", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
